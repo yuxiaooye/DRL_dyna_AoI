0d3fe917ed5a9f44646e1017897981f144959130
Insomnia-y <184035@qq.com>
2023-02-18 23:40:10 +0800

    添加KAIST的额外33个随机走的人 human120-user122.csv

--- source_code/tools/pre/add_random_user.py
+++ source_code/tools/pre/add_random_user.py
@@ -4,17 +4,17 @@ import osmnx as ox
 import numpy as np
 from numpy.random import rand, uniform
 
-poi_num = 50
-default_poi_num = 33
+poi_num = 122
+default_poi_num = 92
 T = 121
 
-rm = Roadmap('NCSU', poi_num=default_poi_num)
-poi_df = pd.read_csv(r'F:\PycharmProjects\jsac\DRL_dyna_AoI\source_code\envs\NCSU\human120.csv')
+rm = Roadmap('KAIST', poi_num=default_poi_num)
+poi_df = pd.read_csv(r'F:\PycharmProjects\jsac\DRL_dyna_AoI\source_code\envs\KAIST\human120.csv')
 
-G = ox.load_graphml(r'F:\PycharmProjects\ICDE\roadmap\data\NCSU\walk\map.graphml')
+G = ox.load_graphml(r'F:\PycharmProjects\ICDE\roadmap\data\KAIST\walk\map.graphml')
 X, Y = rm.max_dis_x, rm.max_dis_y
 for i in range(default_poi_num, poi_num):  # 新生成的user
-    src = np.array([rand() * X, rand() * Y])
+    src = np.array([rand() * (X-200), rand() * (Y-200)])
     src_node = ox.distance.nearest_nodes(G, *rm.pygamexy2lonlat(*src))
     lons, lats = [], []
     r = 24  # 人步行速度1.2m/s
@@ -22,8 +22,9 @@ for i in range(default_poi_num, poi_num):  # 新生成的user
     for t in range(T):
 
         # dst = src + [uniform(-1, 1)*20, uniform(-1, 1)*20]  # 太二了，原地周围随机打转
-        dst = src + [r*np.cos(theta), r*np.sin(theta)]
-        dst = np.clip(dst, [0, 0], [X, Y])  # 出界检测
+        dst_might_out = src + [r*np.cos(theta), r*np.sin(theta)]
+
+        dst = np.clip(dst_might_out, [0+200, 0+200], [X-200, Y-200])  # 出界检测
         dst_node = ox.distance.nearest_nodes(G, *rm.pygamexy2lonlat(*dst))
         lon, lat = G.nodes[dst_node]['x'], G.nodes[dst_node]['y']
         lons.append(lon)
@@ -31,6 +32,8 @@ for i in range(default_poi_num, poi_num):  # 新生成的user
         src = dst
         if rand() < 0.2:  # 以一定概率改变运动朝向
             theta = uniform(0, 2 * np.pi)
+        if not (0+200 < dst_might_out[0] < X-200 and 0+200 < dst_might_out[1] < Y-200):  # 出界就反向移动
+            theta = 2 * np.pi - theta
     pxs, pys = rm.lonlat2pygamexy(np.array(lons), np.array(lats))
     print(1)
 
@@ -40,7 +43,7 @@ for i in range(default_poi_num, poi_num):  # 新生成的user
     poi_df = poi_df.append(subdf)
 
 assert poi_df.shape[0] == poi_num * T
-poi_df.to_csv(rf'F:\PycharmProjects\jsac\DRL_dyna_AoI\source_code\envs\NCSU\human120-user{poi_num}.csv')
+poi_df.to_csv(rf'F:\PycharmProjects\jsac\DRL_dyna_AoI\source_code\envs\KAIST\human120-user{poi_num}.csv')
 
 
 
