786c8fe765e1851041d4c0d9d495f27e75defae9
Insomnia-y <184035@qq.com>
2023-02-21 22:46:36 +0800

    0221night-final

--- source_code/1.sh
+++ source_code/1.sh
@@ -1,13 +1,12 @@
 # !/bin/sh
 source activate yyx_adept
-group='0221-night-ablation'
+group='0221-hyper'
 
-nohup python -u main_DPPO.py --group $group --dataset NCSU --uav_num 5 --aoith 30 --w_noise -104 --algo G2ANet --use_snrmap --knn_coefficient 0.1 --user yyx --device cuda:0 &
-nohup python -u main_DPPO.py --group $group --dataset NCSU --uav_num 5 --aoith 30 --w_noise -104 --algo G2ANet                                    --user yyx --device cuda:1 &
-nohup python -u main_DPPO.py --group $group --dataset NCSU --uav_num 5 --aoith 30 --w_noise -104 --algo DPPO --use_snrmap                         --user yyx --device cuda:2 &
-nohup python -u main_DPPO.py --group $group --dataset NCSU --uav_num 5 --aoith 30 --w_noise -104 --algo DPPO                                      --user yyx --device cuda:3 &
-
-nohup python -u main_DPPO.py --group $group --dataset KAIST --uav_num 5 --aoith 30 --w_noise -104 --algo G2ANet --use_snrmap --knn_coefficient 0.5 --user yyx --device cuda:0 &
-nohup python -u main_DPPO.py --group $group --dataset KAIST --uav_num 5 --aoith 30 --w_noise -104 --algo G2ANet                                    --user yyx --device cuda:1 &
-nohup python -u main_DPPO.py --group $group --dataset KAIST --uav_num 5 --aoith 30 --w_noise -104 --algo DPPO --use_snrmap                         --user yyx --device cuda:2 &
-nohup python -u main_DPPO.py --group $group --dataset KAIST --uav_num 5 --aoith 30 --w_noise -104 --algo DPPO                                      --user yyx --device cuda:3 &
+for g2a_hops in 1 2 3;
+do
+for map_size in 4 6 8 10;
+do
+nohup python -u main_DPPO.py --group $group --dataset NCSU --uav_num 5 --aoith 30 --w_noise -107 --algo G2ANet --use_snrmap --knn_coefficient 0.1 --g2a_hops $g2a_hops --map_size $map_size --user yyx --device cuda:1 &
+nohup python -u main_DPPO.py --group $group --dataset KAIST --uav_num 5 --aoith 30 --w_noise -107 --algo G2ANet --use_snrmap --knn_coefficient 0.1 --g2a_hops $g2a_hops --map_size $map_size --user yyx --device cuda:2 &
+done
+done
